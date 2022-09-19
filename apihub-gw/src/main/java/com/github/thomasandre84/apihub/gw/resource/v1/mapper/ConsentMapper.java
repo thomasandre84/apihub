81f6bfe6472646133be15adc00cb018370bcdaaf
Thomas Andre <thomasandre84@gmail.com>
2022-09-19 22:04:23 +0200

    remove codesmells

--- apihub-gw/src/main/java/com/github/thomasandre84/apihub/gw/resource/v1/mapper/ConsentMapper.java
+++ apihub-gw/src/main/java/com/github/thomasandre84/apihub/gw/resource/v1/mapper/ConsentMapper.java
@@ -4,15 +4,12 @@ import com.github.thomasandre84.apihub.gw.model.Consent;
 import com.github.thomasandre84.apihub.gw.resource.v1.dto.ConsentResponseDto;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.mapstruct.Mappings;
 
 @Mapper(componentModel = "cdi")
 public interface ConsentMapper {
 
-    @Mappings({
-        @Mapping(target = "consentId", source = "id"),
-        @Mapping(target = "providerId", source = "providerId.id"),
-        @Mapping(target = "userId", source = "userId.id")
-    })
+    @Mapping(target = "consentId", source = "id")
+    @Mapping(target = "providerId", source = "providerId.id")
+    @Mapping(target = "userId", source = "userId.id")
     ConsentResponseDto fromConsent(Consent consent);
 }
--- apihub-gw/src/test/java/com/github/thomasandre84/apihub/gw/resource/GreetingResourceIT.java
+++ apihub-gw/src/test/java/com/github/thomasandre84/apihub/gw/resource/GreetingResourceIT.java
@@ -3,7 +3,7 @@ package com.github.thomasandre84.apihub.gw.resource;
 import io.quarkus.test.junit.QuarkusIntegrationTest;
 
 @QuarkusIntegrationTest
-public class GreetingResourceIT extends GreetingResourceTest {
+class GreetingResourceIT extends GreetingResourceTest {
 
     // Execute the same tests but in native mode.
 }
